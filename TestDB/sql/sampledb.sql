64df7be4ed3322d5531d3b3fffb7e185b737d39a
meguuumi23 <35454619+meguuumi23@users.noreply.github.com>
2018-04-11 22:14:18 +0900

    Add files via upload

---
+++ TestDB/sql/sampledb.sql
@@ -0,0 +1,18 @@
+drop database if exists sampledb;
+create sampledb ;
+use sampledb;
+create table sample_table(
+user_id int,
+user_name varchar(50)
+user age int.
+password varchar(50)
+);
+insert into test_table values(1,
+
+"Yamada","21","xxx@yahoo.co.jp");
+insert into test_table values(2,
+
+"Sato","39","yyy@google.com");
+insert into test table values(3,
+"Harada","44","ZZZ@yahoo.co.jp"
+);
